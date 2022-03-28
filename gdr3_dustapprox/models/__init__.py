a095fd5d0ec3632e9df4fc8c131ec51520ba5d8e
Morgan Fouesneau <morgan.fouesneau@astro.washington.edu>
2022-03-28 17:45:06 +0200

    renamed package

--- gdr3_dustapprox/models/__init__.py
+++
@@ -1,9 +0,0 @@
-"""We provide various modeling schemes for extinction in a given photometric band.
-
-.. todo::
-
-    * add script to generate grid of models
-    * add polynomial training.
-    * move literature models to here.
-    * compare literature values to ours.
-"""
