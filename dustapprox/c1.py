184a0754b645663e63e14e12049f6f3a0c0fd5f4
Morgan Fouesneau <morgan.fouesneau@astro.washington.edu>
2022-04-14 12:55:16 +0200

    namings

--- dustapprox/c1.py
+++ dustapprox/c1.py
@@ -161,7 +161,7 @@ import numpy as np
 from .io import ecsv
 
 
-class DR3_extinction:
+class dr3_ext:
     """Function to get the extinction coefficients A_X / A_G in C1M system
 
     The procedure is detailed in the appendix E of `Bellazzini et al. (2022)`_.
